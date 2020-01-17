046cf310c87ead8d36000d9afe166757a349f75e
Humaid AlQassimi <me@humaidq.ae>
2020-01-17 14:22:30 +0000

    models: Add ORM functions for different models/structs.

--- models/device.go
+++ models/device.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"errors"
+)
+
 // DeviceType is the type of smart device.
 type DeviceType int64
 
@@ -19,4 +23,40 @@ type Device struct {
 	Description string
 	Status      bool
 	Temp        float64 `xorm:"null"`
+}
+
+func GetDevice(id int64) (*Device, error) {
+	d := new(Device)
+	has, err := engine.ID(id).Get(d)
+	if err != nil {
+		return d, err
+	} else if !has {
+		return d, errors.New("Device does not exist")
+	}
+	return d, nil
+}
+
+func GetDevices() (devices []Device) {
+	engine.Find(&devices)
+	return
+}
+
+func AddDevice(d *Device) (err error) {
+	_, err = engine.Insert(d)
+	return
+}
+
+func HasDevice(id int64) (has bool) {
+	has, _ = engine.Get(&Device{DeviceID: id})
+	return
+}
+
+func UpdateDevice(d *Device) (err error) {
+	_, err = engine.Id(d.DeviceID).Update(d)
+	return
+}
+
+func UpdateDeviceCols(d *Device, cols ...string) (err error) {
+	_, err = engine.ID(d.DeviceID).Cols(cols...).Update(d)
+	return
 }
--- models/room_stat.go
+++ models/room_stat.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"errors"
+)
+
 // RoomStat represents a room statistic, which is a part of a larger Statistic.
 type RoomStat struct {
 	RStatID     int64 `xorm:"pk autoincr"`
@@ -9,4 +13,40 @@ type RoomStat struct {
 	Temperature float64
 	Humidity    float64
 	OpenWindows int64
+}
+
+func GetRoomStat(id int64) (*RoomStat, error) {
+	r := new(RoomStat)
+	has, err := engine.ID(id).Get(r)
+	if err != nil {
+		return r, err
+	} else if !has {
+		return r, errors.New("RoomStat does not exist")
+	}
+	return r, nil
+}
+
+func GetRoomStats() (roomStats []RoomStat) {
+	engine.Find(&roomStats)
+	return
+}
+
+func AddRoomStat(r *RoomStat) (err error) {
+	_, err = engine.Insert(r)
+	return
+}
+
+func HasRoomStat(id int64) (has bool) {
+	has, _ = engine.Get(&RoomStat{RStatID: id})
+	return
+}
+
+func UpdateRoomStat(r *RoomStat) (err error) {
+	_, err = engine.Id(r.RStatID).Update(r)
+	return
+}
+
+func UpdateRoomStatCols(r *RoomStat, cols ...string) (err error) {
+	_, err = engine.ID(r.RStatID).Cols(cols...).Update(r)
+	return
 }
