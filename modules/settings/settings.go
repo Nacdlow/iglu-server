6a6b608fe37d886f2f045576847c43bc70cb15b0
Humaid AlQassimi <me@humaidq.ae>
2020-01-17 16:38:53 +0000

    Config: Add engineer, customer id, etc.

--- modules/settings/settings.go
+++ modules/settings/settings.go
@@ -2,6 +2,7 @@ package settings
 
 import (
 	"github.com/spf13/viper"
+	"gitlab.com/skilstak/code/go/uniq"
 )
 
 var (
@@ -14,8 +15,15 @@ func LoadConfig() {
 	Config.AddConfigPath("$HOME/.config/nacdlow")
 	Config.AddConfigPath(".")
 
+	Config.SetDefault("Port", "8080")
 	Config.SetDefault("HouseName", "My House")
 	Config.SetDefault("Address", "Heriot-Watt University, Edinburgh, Scotland. EH14 4AS")
+	Config.SetDefault("Engineer.Name", "Nacdlow Support")
+	Config.SetDefault("Engineer.Phone", "074 123 4567")
+	Config.SetDefault("Engineer.Email", "support@nacdlow.com")
+	if !Config.IsSet("CustomerID") {
+		Config.SetDefault("CustomerID", uniq.UUID())
+	}
 	//Config.SetDefault("","")
 
 	if err := Config.ReadInConfig(); err != nil {
