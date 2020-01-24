ac109b5d1ea7ce6c60f58f21790c9b4e326314d3
Humaid AlQassimi <me@humaidq.ae>
2020-01-24 13:49:29 +0000

    Add weather as separate module

---
+++ modules/weather/weather.go
@@ -0,0 +1,19 @@
+package weather
+
+import (
+	"github.com/adlio/darksky"
+	"gitlab.com/group-nacdlow/nacdlow-server/modules/settings"
+)
+
+// GetWeather loads the forecast from Darksky's API.
+func GetWeather() (*darksky.Forecast, error) {
+	lat := settings.Config.GetString("Location.Lat")
+	lon := settings.Config.GetString("Location.Lon")
+	client := darksky.NewClient(settings.Config.GetString("DarkskyAPIKey"))
+	f, err := client.GetForecast(lat, lon,
+		darksky.Arguments{"units": "si", "extend": "hourly"})
+	if err != nil {
+		return nil, err
+	}
+	return f, nil
+}
