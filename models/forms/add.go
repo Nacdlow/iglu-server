8d2a3492c30b0b6fcd05c95a12758b9ff69b5893
Humaid AlQassimi <me@humaidq.ae>
2020-01-26 17:59:58 +0000

    Add binding forms for adding rooms and devices.

---
+++ models/forms/add.go
@@ -0,0 +1,17 @@
+package forms
+
+// AddRoomForm represents a form to add a new Room to the house.
+type AddRoomForm struct {
+	RoomName     string `form:"room_name" binding:"Required"`
+	Description  string `form:"description" binding:"Required"`
+	RoomType     int64  `form:"room_type"` // We don't use required because that would require number >0
+	WindowsCount int64  `form:"windows_count"`
+}
+
+// AddDeviceForm represents a form to add a new Device to a room.
+type AddDeviceForm struct {
+	RoomID      int64  `form:"room_id"`
+	DeviceType  int64  `form:"device_type"`
+	Description string `form:"description" binding:"Required"`
+	IsMainLight bool   `form:"is_main_light_source"`
+}
