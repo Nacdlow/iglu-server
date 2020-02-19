475208a61ac3f1e372e2f76455f0fc708fc9127c
numan ali <nman786@gmail.com>
2020-02-19 22:32:52 +0000

    no back arrow on rooms page

--- routes/main.go
+++ routes/main.go
@@ -222,6 +222,7 @@ func SpecificRoomsHandler(ctx *macaron.Context) {
 		ctx.Data["Devices"] = models.GetDevices()
 	}
 
+	ctx.Data["ArrowBack"] = 1
 	ctx.Data["IsRooms"] = 1
 	ctx.HTML(200, "specificRooms")
 }
