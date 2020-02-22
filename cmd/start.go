b5cdbc8f5935105dc432d64df24df916c5495141
Ruaridh Mollica <ruaridhmollica@gmail.com>
2020-02-22 19:01:49 +0000

    options elipses added to rooms - will add remove function later

--- cmd/start.go
+++ cmd/start.go
@@ -107,6 +107,7 @@ func getMacaron() *macaron.Macaron {
 
 		m.Get("/toggle_fave/:id", routes.FaveHandler)     //set device as fave
 		m.Get("/remove_device/:id", routes.RemoveHandler) //remove a device
+		m.Get("/remove_room/:id", routes.RemoveRoomHandler)//removes a room
 
 		m.Group("", func() {
 			m.Group("/add", func() {
