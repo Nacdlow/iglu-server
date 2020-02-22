3c79033e30a7af558ad4b90ed2043bc0b870cb40
melanthios <alakbar234@gmail.com>
2020-02-22 22:21:54 +0000

    Cleaned up alert.go

--- models/alert.go
+++ models/alert.go
@@ -1,25 +1,22 @@
 package models
 
-import (
-	"time"
-)
-
 // Category is a ranking of how severe the aleart is
 type Category int64
 
 // Category enums from lowest to highest.
 const (
-	Low 			// 0
-	Medium 			// 1
-	High        	// 2
+	Low    = iota // 0
+	Medium        // 1
+	High          // 2
 )
 
-//Alert struct represents the Aleart title, AlertCategory (severity), Alert Message,
+//Alert struct represents the Alert title, AlertCategory (severity), Alert Message,
 // and also the time the Alert was sent to the user
 type Alert struct {
-	AlertID int64	`xorm:"pk autoincr"`
-	AlertCategory Category
-	Time int64
-	AlertTitle string
-	AlertMessage string
+	AlertID         int64 `xorm:"pk autoincr"`
+	Time            int64 `xorm:"index"`
+	Username        string
+	AlertTitle      string
+	AlertMessage    string
+	AlertImportance Category
 }
